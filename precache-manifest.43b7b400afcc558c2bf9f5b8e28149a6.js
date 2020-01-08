80bdf040c22b794a75c92dfa90dadba6ebdff6ad
Denis Chumakov <chumakov@azoft.com>
2020-01-08 18:01:43 +0700

    'deploy'

---
+++ precache-manifest.43b7b400afcc558c2bf9f5b8e28149a6.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/pairs-1/images/YouTube.svg"
+  },
+  {
+    "revision": "4c4f2a16c766f32cf8b31bec089bbcde",
+    "url": "/events/open-1/photos/1st.png"
+  },
+  {
+    "revision": "da6b0dbfcf899ef78b4d3d2e1e6ab4d7",
+    "url": "/events/open-1/photos/3rd.png"
+  },
+  {
+    "revision": "1c83dc80f6944cbf4367",
+    "url": "/js/app.6aae4ecc.js.map"
+  },
+  {
+    "revision": "b0bceb49f4331d5e6d62014112748c6d",
+    "url": "/img/tt22.png"
+  },
+  {
+    "revision": "b1f7c2b8058869821ea6",
+    "url": "/js/chunk-vendors.3d586e8a.js"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b1f7c2b8058869821ea6",
+    "url": "/js/chunk-vendors.3d586e8a.js.map"
+  },
+  {
+    "revision": "8c6de712ea9aac1bca65569b2e91bda8",
+    "url": "/img/blue-yellow.8c6de712.png"
+  },
+  {
+    "revision": "94a163192d2f5157f517d54234212336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8e4a3852e1d87264efe3ef5dcbcbe19a",
+    "url": "/img/YouTube.svg"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/pairs-1/images/azoft.svg"
+  },
+  {
+    "revision": "1c83dc80f6944cbf4367",
+    "url": "/js/app.6aae4ecc.js"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/08.01.2020/images/azoft.svg"
+  },
+  {
+    "revision": "21f4752eb889d7f7ec3bab0f7359f3fa",
+    "url": "/events/open-1/images/one.inc.png"
+  },
+  {
+    "revision": "0eafd988d1247988f964191fd7f1e21b",
+    "url": "/events/open-1/images/itk.png"
+  },
+  {
+    "revision": "5c0df5e96307287fbc103a3acc41a6f5",
+    "url": "/events/open-1/images/ds1.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/open-1/images/azoft.svg"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/open-1/images/YouTube.svg"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/08.01.2020/images/YouTube.svg"
+  },
+  {
+    "revision": "b1f7c2b8058869821ea6",
+    "url": "/css/chunk-vendors.8e8fb12b.css.map"
+  },
+  {
+    "revision": "b1f7c2b8058869821ea6",
+    "url": "/css/chunk-vendors.8e8fb12b.css"
+  },
+  {
+    "revision": "1c83dc80f6944cbf4367",
+    "url": "/css/app.6462e20c.css.map"
+  },
+  {
+    "revision": "1c83dc80f6944cbf4367",
+    "url": "/css/app.6462e20c.css"
+  },
+  {
+    "revision": "64cebf6b63e746db74a06454be29829d",
+    "url": "/CNAME"
+  }
+];
